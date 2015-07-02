f77cb3b126255e344de57a0bc24b8f0a9e0c70e1
NirbhayJ <nirbhay44@gmail.com>
2015-07-02 12:21:18 -0400

    created

---
+++ midterm/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace midterm.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ midterm/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(midterm.Startup))]
+namespace midterm
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
